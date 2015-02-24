01a82d6bc11c11d1219765d27640861985151ed8
Christoph Martel <chris@codeways.org>
2015-02-24 21:44:49 +0100

    basic authentification

--- server.js
+++ server.js
@@ -4,19 +4,25 @@ const
   express = require('express'),
   app = express(),
   logger = require('morgan'),
+  flash = require('connect-flash'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
   handlebars = require('express-handlebars'),
   bodyParser = require('body-parser');
   app.engine('.hbs', handlebars({extname: '.hbs'}));
   app.set('view engine', '.hbs');
+  app.use(cookieParser('secret'));
+  app.use(session({cookie: { maxAge: 60000 }}));
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/node_modules/bootstrap'));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(flash())
 
 app.get('/', function(req, res, next) {
-  res.send("root page");
+    res.render('home', {layout: 'main', messages: req.flash('test')});
 })
 
 require('./lib/auth.js')(app);
